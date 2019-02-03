5b7c632f75596f4c2a03513742a507823b343371
Sim4n6 <sim4n6@gmail.com>
2019-02-03 16:41:41 +0100

    added support for a testSuite for all testCases and test_methods

---
+++ tests/main_test.py
@@ -0,0 +1,17 @@
+from unittest import TextTestRunner, TestSuite
+
+from tests.test_csv_manip import TestCsvManip
+from tests.test_xlsx_manip import TestXlsxManip
+
+if __name__ == '__main__':
+
+	# define a test Suite
+	suite = TestSuite()
+	suite.addTest(TestCsvManip("test_to_csv"))
+	suite.addTest(TestCsvManip("test_from_csv"))
+	suite.addTest(TestXlsxManip("test_write_job_offer_2_worksheet"))
+	suite.addTest(TestXlsxManip("test_create_xlsx_dir"))
+
+	# instantiate the test runner
+	runner = TextTestRunner()
+	runner.run(suite)
