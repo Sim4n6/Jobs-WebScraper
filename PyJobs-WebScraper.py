08d9421f6ff0b359081fa62784980333e68eda5a
Sim4n6 <Sim4n6@gmail.com>
2019-02-05 12:55:40 +0000

    no web scraping from python.org twice

--- PyJobs-WebScraper.py
+++ PyJobs-WebScraper.py
@@ -7,7 +7,7 @@ import feedparser
 # user modules
 from common.Decorators import log_decorator
 from common.Xlsx_manip import save_to_xlsx
-from common.csv_manip import from_csv, to_csv
+from common.csv_manip import from_csv, to_csv, is_csv_exist
 from web_scrape.web_scrape_board import extract_job_content, web_scrape, is_allowed_by_robot
 from web_scrape.web_scrape_feed import extract_job_offer_from_feed, print_feed_infos
 
@@ -16,16 +16,20 @@ from web_scrape.web_scrape_feed import extract_job_offer_from_feed, print_feed_i
 def web_scrape_demo(location, url_2_scrape):
 	""" Web scrape the location, extract the job offer urls and then store to xlsx """
 
-	urls_from_csv = from_csv("scraped_urls__" + location + ".csv")
 	urls = set()
 	url = ""
+	if is_csv_exist("./saved_jobs/" + "scraped_urls__" + location + ".csv"):
+		urls_from_csv = from_csv("./saved_jobs/" + "scraped_urls__" + location + ".csv")
+	else:
+		urls_from_csv = set()
+
 	job_urls = web_scrape(location, url_2_scrape)
+
 	if url in job_urls and url not in urls_from_csv:
 		urls.add(url)
 
 	list_job_offers = extract_job_content(urls)
 
-	# FIXME in case urls contain something it need to be added to csv , try it now
 	for url in urls:
 		job_urls.append(url)
 	to_csv(job_urls, "scraped_urls__" + location + ".csv")
